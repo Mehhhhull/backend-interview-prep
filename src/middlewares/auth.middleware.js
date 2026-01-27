56c62d7f050b927fc53bb2d1a254d9ed8fdfa967
Mehhhhull <mehulkumarsingh.2004@gmail.com>
2026-01-27 20:45:41 +0530

    Using Try Catch

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -6,21 +6,25 @@ import jwt from "jsonwebtoken"
 import { User } from "../models/user.model.js";
 
 export const verifyJWT=asyncHandler(async(req,res,next)=>{
-  const token=req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
-
-  if (!token) {
-    throw new ApiError(401,"Not authorized, token missing")
+  try {
+    const token=req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+  
+    if (!token) {
+      throw new ApiError(401,"Not authorized, token missing")
+    }
+   
+    const decodedToken=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+  
+    const user=await User.findById(decodedToken?._id).select("-password -refreshToken")
+  
+    if(!user){
+      throw new ApiError(401,"Invalid Acccess Token")
+    }
+  
+    req.user=user
+    next()
+  } catch (error) {
+    throw new ApiError(401,error?.message||"Invalid Access Token")
   }
- 
-  const decodedToken=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
-
-  const user=await User.findById(decodedToken?._id).select("-password -refreshToken")
-
-  if(!user){
-    throw new ApiError(401,"Invalid Acccess Token")
-  }
-
-  req.user=user
-  next()
   
 })
